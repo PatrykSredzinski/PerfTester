fb88b5ff0124d2b4d13b21e1fbe70380b659ceaa
Patryk Średziński <>
2018-05-06 11:48:16 +0200

    Xcode Project Init

---
+++ PerfTester-Xcode/PerfTester-Xcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfTester-Xcode
+//
+//  Created by Patryk Średziński on 06.05.2018.
+//  Copyright © 2018 private.perftesterxcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
