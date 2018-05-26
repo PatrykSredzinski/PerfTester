96d8a5f4af5faf9e333a3d6910ea095f3f41ff9d
Patryk Średziński <>
2018-05-26 18:35:38 +0200

    Realm is now working on iPhone device

--- PerfTester-Xamarin/PerfTester-Xamarin/Main.cs
+++ PerfTester-Xamarin/PerfTester-Xamarin/Main.cs
@@ -1,4 +1,5 @@
-﻿using UIKit;
+﻿using System;
+using UIKit;
 
 namespace PerfTesterXamarin
 {
@@ -7,8 +8,8 @@ namespace PerfTesterXamarin
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            Realms.RealmConfigurationBase.GetPathToRealm(documentsPath);
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
